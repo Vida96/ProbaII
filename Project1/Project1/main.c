708d9e8e639bd20c361e542478b904292d1a964c
Milan Carakovac <milancarakovac@yahoo.com>
2016-12-26 16:26:40 +0100

    Drugi

--- Project1/Project1/main.c
+++ Project1/Project1/main.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
+int sum(int a, int b)
+{
+	return a + b;
+}
 
 int main()
 {
-	printf("Prvi pokusaj!!!\n");
+	printf("Sume oblika (i+2)+(2i+1):\ni=(0,9)\n");
+	for (int i = 0; i < 10; i++)
+		printf("%d \n", sum(i + 2, 2 * i + 1));
 	system("pause");
 	return 0;
 }
